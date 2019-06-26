792b7ebac064333ebe25c583cbf981fb3f1b8c55
Mike Shauneu <mike.shauneu@oath.com>
2019-06-26 14:08:37 -0400

    flutter 1.5.4-hotfix.2

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,5 +1,5 @@
 cask "flutter" do
-    version "v1.5.4-hotfix.2"
+    version "1.5.4-hotfix.2"
     sha256 '6a2554c3754322848aa6b38e449c42d5aa9149ea82bf97f168782f68a8efa0b1'
 
     url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v#{version}-stable.zip"

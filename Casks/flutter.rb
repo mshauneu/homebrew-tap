738fc0b3c4e513b7bbf7fbc4b46f4e94842231a2
Mike Shauneu <m.shauneu@gmail.com>
2019-09-12 13:56:58 -0400

    fix version tag

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,7 +1,6 @@
 cask "flutter" do
-    version "1.9.1-hotfix.2"
+    version "1.9.1+hotfix.2"
     sha256 '24433d53579ab60a70f969e39ff7b30484dc9830f95590d71f3318d8955acad1'
-
     url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v#{version}-stable.zip"
     name "flutter"
     homepage "https://flutter.dev"
